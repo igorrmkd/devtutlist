43bcf9cec8d9dbcdf9e21e08434f2fa25501c951
Igor Risteski <pirej30@yahoo.com>
2021-03-10 00:27:14 +0100

    Add some error validation using try/catch

--- server/routers/tutorialRouter.js
+++ server/routers/tutorialRouter.js
@@ -3,23 +3,27 @@ const Tutorial = require("../models/tutorialModel");
 const router = Router();
 
 router.post("/", async (req, res) => {
-  const { img, title, description } = req.body;
-  // validation
-  if (!title && !description) {
-    return res.status(400).json({
-      errorMessage: "you must put Title and Description",
+  try {
+    const { img, title, description } = req.body;
+    // validation
+    if (!title && !description) {
+      return res.status(400).json({
+        errorMessage: "you must put Title and Description",
+      });
+    }
+
+    const newTutorial = new Tutorial({
+      img,
+      title,
+      description,
     });
+
+    const savedTutorial = await newTutorial.save();
+
+    res.json(savedTutorial);
+  } catch (error) {
+    res.status(500).send();
   }
-
-  const newTutorial = new Tutorial({
-    img,
-    title,
-    description,
-  });
-
-  const savedTutorial = await newTutorial.save();
-
-  res.json(savedTutorial);
 });
 
 module.exports = router;
