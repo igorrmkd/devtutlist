e5bd3b226ef682f37b4517a2a273afa03278de05
Igor Risteski <pirej30@yahoo.com>
2021-03-09 12:54:58 +0100

    create initial base express server and connect it to mongodb-Atlas

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+// express server
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5000"],
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// connect to mongodbAtlas
+mongoose.connect(
+  process.env.MDB_CONNECT_STRING,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+  err => {
+    if (err) return console.error(err);
+    console.log("Connected to MongoDB");
+  }
+);
