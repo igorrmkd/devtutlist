bfd22398726ba469b1666acb421c95ef7702e4aa
Igor Risteski <igorrmkd@outlook.com>
2021-03-10 01:01:30 +0100

    DELETE the tutorials from the database

--- server/routers/tutorialRouter.js
+++ server/routers/tutorialRouter.js
@@ -35,4 +35,29 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const tutorialId = req.params.id; // identify the tutorial by id
+
+    if (!tutorialId)
+      // if there is no tutorial id..
+      return res.status(400).json({
+        errorMessage: "Tutorial id not supplied. Please contact the developer",
+      });
+
+    const existingTutorial = await Tutorial.findById(tutorialId); // wait for the tutorial set to be deleted
+    if (!existingTutorial)
+      // if there is no tutorial with such ID..
+      return res.status(400).json({
+        errorMessage:
+          "No tutorial with this ID is found. Please contact the developer",
+      });
+
+    await existingTutorial.delete(); // delete the tutorial
+    // res.json(existingTutorial); //if you want to show the deleted tutorial
+  } catch (err) {
+    res.status(500).send();
+  }
+});
+
 module.exports = router;
