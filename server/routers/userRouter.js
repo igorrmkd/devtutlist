ed1da61f6baaeee631c89efffa09fa09297903a5
Igor Risteski <igorrmkd@outlook.com>
2021-03-17 03:59:42 +0100

    Basic registration input validation

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,7 +1,26 @@
 const router = require("express").Router();
 
 router.post("/", async (req, res) => {
-  res.send("it works");
+  try {
+    const { email, password, passwordVerify } = req.body;
+    // basic validation
+    if (!email || !password || !passwordVerify)
+      return res.status(400).json({
+        errorMessage: "Please enter all required fields",
+      });
+
+    if (password.length < 6)
+      return res.status(400).json({
+        errorMessage: "Please enter a password of at least 6 characters",
+      });
+
+    if (password !== passwordVerify)
+      return res.status(400).json({
+        errorMessage: "Please enter the same password twice for verification",
+      });
+  } catch (err) {
+    res.status(500).send();
+  }
 });
 
 module.exports = router;
