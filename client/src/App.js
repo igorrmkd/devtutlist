b34e65d62579d875b46399966ccbd2448d2e0182
Igor Risteski <igorrmkd@outlook.com>
2021-03-19 01:42:14 +0100

    Context - part 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import axios from "axios";
+import { UserContextProvider } from "./context/UserContext";
 import Router from "./Router";
 
 axios.defaults.withCredentials = true;
 
 function App() {
-  return <Router />;
+  return (
+    <UserContextProvider>
+      <div>
+        <Router />
+      </div>
+    </UserContextProvider>
+  );
 }
 
 export default App;
---
+++ client/src/context/UserContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+const UserContext = createContext();
+
+function UserContextProvider(props) {
+  const [user, setUser] = useState(undefined);
+
+  async function getUser() {
+    const userRes = await axios.get("http://localhost:5000/auth/loggedin/");
+    setUser(userRes.data);
+  }
+
+  useEffect(() => {
+    getUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <UserContext.Provider value={user}>{props.children}</UserContext.Provider>
+  );
+}
+
+export { UserContextProvider };
