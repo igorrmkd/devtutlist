cc5db1be31ed5506a31f2c92e379daee527536ec
Igor Risteski <igorrmkd@outlook.com>
2021-03-20 01:05:59 +0100

    hide the log in and register buttons if already logged in

--- client/src/components/misc/Navbar.js
+++ client/src/components/misc/Navbar.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import UserContext from "../../context/UserContext";
 
 const Navbar = () => {
+  const { user } = useContext(UserContext);
+
   return (
     <div>
       <Link to="/">
         <h1>WebDev tutorials</h1>
       </Link>
-      <Link to="/login">Log in</Link>
-      <Link to="/register">Register</Link>
+      {!user && (
+        <>
+          <Link to="/login">Log in</Link>
+          <Link to="/register">Register</Link>
+        </>
+      )}
     </div>
   );
 };
