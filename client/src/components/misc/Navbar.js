f66e4c1a9c5fcd0710e39d002540924ea9abedb3
Igor Risteski <igorrmkd@outlook.com>
2021-03-20 02:02:45 +0100

    hide the edit-tutorial button when logged out

--- client/src/components/misc/Navbar.js
+++ client/src/components/misc/Navbar.js
@@ -4,10 +4,11 @@ import { Link } from "react-router-dom";
 import UserContext from "../../context/UserContext";
 
 const Navbar = () => {
-  const { user } = useContext(UserContext);
+  const { user, getUser } = useContext(UserContext);
 
   async function logout() {
     await axios.get("http://localhost:5000/auth/logout");
+    await getUser();
   }
 
   return (
