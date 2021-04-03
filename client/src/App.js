39d82e026da33f4d4e70b458e4f36c44cb146a7c
Igor Risteski <igorrmkd@outlook.com>
2021-04-03 15:43:01 +0200

    NORMALIZE Scss and some small screen tweaks

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { UserContextProvider } from "./context/UserContext";
 import Router from "./Router";
+import "./style/normalize.scss";
 import "./style/index.scss";
 
 axios.defaults.withCredentials = true;
